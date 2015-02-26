bb3051401a199e6be7d974f067b3322511185818
Duane Moody and Lee Jorgensen <leejorgensen22@gmail.com>
2015-02-26 13:57:58 -0800

    add change for all point values when button is clicked

--- controllers/CountController.js
+++ controllers/CountController.js
@@ -1,5 +1,10 @@
 hackNewsClone.controller('CountCtrl', function CountCtrl($scope) {
-    $scope.addOneVote = function(id) {
+    $scope.addOneVote = function(id, article) {
       $scope.articles[id-1].count += 1;
-    }
+      for(i=0; i < article.length; i++){
+        $scope.articles[i].points = $scope.articles[i].count * (($scope.articles[i].time / Date.now()));
+      }
+    };
+
+        // $scope.articles[id-1].points = $scope.articles[id-1].count * (($scope.articles[id-1].time / Date.now()));
 });
--- services/ArticleFactory.js
+++ services/ArticleFactory.js
@@ -2,7 +2,7 @@ hackNewsClone.factory('ArticleFactory', function ArticleFactory(){
   var factory = {};
   factory.articles = [];
   factory.addArticle = function() {
-    factory.articles.push({ name: factory.articleName, link: factory.articleLink, id: factory.articles.length + 1, comments: [], count: 0 });
+    factory.articles.push({ name: factory.articleName, link: factory.articleLink, id: factory.articles.length + 1, comments: [], count: 0, points: 0, time: Date.now() });
     factory.articleName = null;
     factory.articleLink = null;
   };
