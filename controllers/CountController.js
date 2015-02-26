21f5499a7d431e1e51d28ebed86ca3d11c90639e
Duane Moody and Lee Jorgensen <leejorgensen22@gmail.com>
2015-02-26 14:29:59 -0800

    add better depriciation overtime

--- controllers/CountController.js
+++ controllers/CountController.js
@@ -2,7 +2,8 @@ hackNewsClone.controller('CountCtrl', function CountCtrl($scope) {
     $scope.addOneVote = function(id, article) {
       $scope.articles[id-1].count += 1;
       for(i=0; i < article.length; i++){
-        $scope.articles[i].points = $scope.articles[i].count * (($scope.articles[i].time / Date.now()));
+        var timepassed = (Date.now() - $scope.articles[i].time)*.00001;
+        $scope.articles[i].points = $scope.articles[i].count - (timepassed);
       }
     };
 
