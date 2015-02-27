e7c8d2180fcdcfd50044b078bdbca4d16cdeede8
Duane Moody and Lee Jorgensen <dmmoody@gmail.com>
2015-02-26 16:01:36 -0800

    added some pretty colors

--- controllers/CountController.js
+++ controllers/CountController.js
@@ -3,9 +3,7 @@ hackNewsClone.controller('CountCtrl', function CountCtrl($scope) {
       $scope.articles[id-1].count += 1;
       for(i=0; i < article.length; i++){
         var timepassed = (Date.now() - $scope.articles[i].time)*.00001;
-        $scope.articles[i].points = $scope.articles[i].count - (timepassed);
+        $scope.articles[i].points = ($scope.articles[i].count - (timepassed)) + 8 + $scope.articles[i].comments.length;
       }
     };
-
-        // $scope.articles[id-1].points = $scope.articles[id-1].count * (($scope.articles[id-1].time / Date.now()));
 });
--- controllers/PointController.js
+++
@@ -1,8 +0,0 @@
-hackNewsClone.controller('PointsCtrl', function PointsCtrl($scope, ArticleFactory, CommentFactory){
-  $scope.articles = ArticleFactory.articles;
-
-  $scope.caculatePoints = function(id){
-    percent = $scope.articles[id-1].time / Date.now()
-    $scope.articles[id-1].points = $scope.articles[id-1].count;
-  }
-});
